170b59fc49f747230414e15a893b4b7673381773
Nair Palacios <nairpalacios@live.com>
2023-03-27 19:00:30 -0300

    fix: fixes rollup scss error

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -31,6 +31,6 @@ export default [
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
-        external: [/\.css$/, "react", "react-dom"]
+        external: [/\.scss$/, "react", "react-dom"]
     }
 ];
